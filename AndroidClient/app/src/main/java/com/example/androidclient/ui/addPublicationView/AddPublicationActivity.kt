146f42022771f13b1a3127b8cbd411d2f414e51a
Maciej Kozłowski <maciekkoz98@gmail.com>
2019-12-29 00:07:48 +0100

    add publication view

---
+++ AndroidClient/app/src/main/java/com/example/androidclient/ui/addPublicationView/AddPublicationActivity.kt
@@ -0,0 +1,13 @@
+package com.example.androidclient.ui.addPublicationView
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.androidclient.R
+
+class AddPublicationActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_publication)
+    }
+}
--- AndroidClient/app/src/main/java/com/example/androidclient/ui/publicationsView/PublicationsActivity.kt
+++ AndroidClient/app/src/main/java/com/example/androidclient/ui/publicationsView/PublicationsActivity.kt
@@ -1,11 +1,14 @@
 package com.example.androidclient.ui.publicationsView
 
+import android.content.Intent
 import android.os.Bundle
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.androidclient.R
+import com.example.androidclient.ui.addPublicationView.AddPublicationActivity
+import com.example.androidclient.ui.login.LOGIN
 
 import kotlinx.android.synthetic.main.activity_pubs.*
 
@@ -13,11 +16,13 @@ class PublicationsActivity : AppCompatActivity() {
     private lateinit var recyclerView: RecyclerView
     private lateinit var viewAdapter: RecyclerView.Adapter<*>
     private lateinit var viewManager: RecyclerView.LayoutManager
+    private var user: String? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_pubs)
         setSupportActionBar(toolbar)
+        user = intent.getStringExtra(LOGIN)
 
         //TODO get publicationsDataset
         val macPub = Publication("mactitle", "macAuthor", "macPub", "1234")
@@ -33,9 +38,11 @@ class PublicationsActivity : AppCompatActivity() {
             adapter = viewAdapter
         }
 
-        fab.setOnClickListener { view ->
-            Snackbar.make(view, "Tu będzie Dodaj Publikację!", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
+        fab.setOnClickListener {
+            val intent = Intent(this, AddPublicationActivity::class.java).apply {
+                putExtra(LOGIN, user)
+            }
+            startActivity(intent)
         }
     }
 
