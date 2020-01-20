0a62b67eeb54c80a7fffc204308d5c49345189f0
Maciej Kozlowski <maciekkoz98@gmail.com>
2020-01-20 09:41:30 +0100

    cleaning logs and comments

--- web/app/static/handlePopups.js
+++ web/app/static/handlePopups.js
@@ -4,7 +4,6 @@ function popupsHandler() {
     var source = new EventSource('/stream');
     var out = document.getElementById('out');
     source.onmessage = function (e) {
-        console.log(e.data);
         out.setAttribute("class", "visible");
         out.innerHTML = "Dodano nową publikację o tytule " + e.data + "\n" + "Odśwież by ją zobaczyć!\n" + out.innerHTML;
     };
